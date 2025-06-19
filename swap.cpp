c6e26e8e414011ffccb8f993deb090d7228dd2ff
OmSingh5131 <154455357+OmSingh5131@users.noreply.github.com>
2025-06-20 01:10:38 +0530

    Swap adjacent positions of the array
    In this problem alternate position of the array is swapped

---
+++ swap.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+void print_array(int arr[], int size){
+    for( int i = 0; i < size ; i = i + 1){
+        cout << arr[i];
+
+    }
+
+    cout << endl;
+}
+
+void alternate_swap(int arr [], int size){
+
+    if( size % 2 == 0 ){
+        for( int i = 0; i < size;  i = i + 2){
+            swap(arr[i],arr[i + 1]);
+            print_array( arr, size );
+        }
+    }else{
+        for(int i = 0; i < size -1 ; i = i + 2){
+            swap(arr[i],arr[i+1]);
+            print_array( arr , size);
+        }
+
+    }
+
+}
+
+int main(){
+    int arr[ 100 ];
+    int size;
+
+    cout << "Enter the Size of your array : ";
+    cin >> size;
+    cout << endl;
+
+    for(int i =0; i < size; i++){
+        cout << "Enter the element " << i << " of the array : ";
+        cin >> arr[i];
+
+    }
+    
+    alternate_swap(arr,size);
+
+
+}
